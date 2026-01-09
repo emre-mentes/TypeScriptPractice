b42dff9365146815195a779a1d97f598b7a05064
Emre MENTES <emreowen57@gmail.com>
2026-01-09 23:57:51 +0100

    changes

--- day01/01-first-codes.ts
+++ day01/01-first-codes.ts
@@ -1,3 +1,14 @@
-console.log("Hallo TypeScript");
-console.log("Hallo World");
-console.log("Durdurmak icin 'ctrl + c ' ye basabiliriniz")
+console.log("TypeScript derslerimize hosgeldiniz");
+console.log("hello world");
+console.log("durdurmak icin ctrl + c ye basabilirsiniz")
+
+//typescript dosyamizi otomatik olarak calistirir ve dosyada yapilan değisiklikleri izler
+//değisiklik oldugunda otomatik olarak tekrar kodlari calistirir
+// npx tsx --watch .\day01\01-first-codes.ts
+console.log("hello");
+
+// tek satirlik yorum satiri icin CTRL + Ç
+/*
+Birden fazla satiri yorum yapmak icin
+Shift + Alt + A
+*/
