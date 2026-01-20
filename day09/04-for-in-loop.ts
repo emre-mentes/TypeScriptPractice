6c0f195548b94403b7ebaeb1c585f04ed1b937af
Emre MENTES <emreowen57@gmail.com>
2026-01-20 08:32:38 +0100

    changes

---
+++ day09/04-for-in-loop.ts
@@ -0,0 +1,17 @@
+/*
+SYNTAX :
+
+for (let index in arrayname) {
+// calısması istenen kodlar 
+}
+
+*/
+const meyveler: string[] = ["elma", "armut", "muz", "kivi"];
+
+for (let index in meyveler) {
+
+    console.log(index);//Elemanin index nosunu yazdirir
+
+    console.log(meyveler[index]);
+    
+}
